b51c0ea85c5effb5d27f1d49fc8eab421b5796df
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2022-07-28 00:37:34 +0600

    routing with page completed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+const Home = () => {
+  return <h1>Hello World</h1>;
+};
 
-export default function Home() {
-  return <div className={styles.container}>Hello World</div>;
-}
+export default Home;
