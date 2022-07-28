7c78db791f948d6bfc24d554b4545ae3548c086d
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2022-07-28 07:41:42 +0600

    dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 const Home = () => {
-  return <h1>Hello World</h1>;
+  const router = useRouter();
+  const goToProductPage = () => {
+    router.push("/product");
+  };
+
+  return (
+    <div>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <div onClick={() => goToProductPage()}>
+        <h1>Products</h1>
+      </div>
+      <h1>Hello World</h1>
+    </div>
+  );
 };
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
 export default function Products() {
   return (
     <div>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <Link href="/product/product1">
+        <a>Product 1</a>
+      </Link>
+      <Link href="/product/product2">
+        <a>Product 2</a>
+      </Link>
+      <Link href="/product/product3">
+        <a>Product 3</a>
+      </Link>
     </div>
   );
 }
