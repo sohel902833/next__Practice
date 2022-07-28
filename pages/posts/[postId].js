1f8cc3f20283ff021cc0379e0e0d688f0956aee8
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2022-07-28 11:12:07 +0600

    static generation with fallback blocking

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+
 export default function PostDetails({ post }) {
   return (
     <div>
@@ -13,10 +15,15 @@ export async function getStaticProps(context) {
   const { params } = context;
 
   const res = await fetch(
-    "https://jsonplaceholder.typicode.com/posts/" + params.postId
+    "https://jsonplaceholder.typicode.com/posts/" + params?.postId
   );
   const data = await res.json();
-
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+  console.log("Generating page for /posts/" + params?.postId);
   return {
     props: {
       post: data,
@@ -25,15 +32,25 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
-  const data = await res.json();
-  const paths = data.map((post) => {
-    return {
-      params: { postId: `${post.id}` },
-    };
-  });
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+  //   const data = await res.json();
+  //   const paths = data.map((post) => {
+  //     return {
+  //       params: { postId: `${post.id}` },
+  //     };
+  //   });
   return {
-    paths,
-    fallback: false,
+    paths: [
+      {
+        params: { postId: "1" },
+      },
+      {
+        params: { postId: "2" },
+      },
+      {
+        params: { postId: "3" },
+      },
+    ],
+    fallback: "blocking",
   };
 }
